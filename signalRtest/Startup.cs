642fd2f6e38d35bf0123b2da03271496092cea29
oncell <onselaydin@gmail.com>
2017-12-18 00:26:21 +0300

    first checkin

---
+++ signalRtest/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly:OwinStartup(typeof(signalRtest.Startup))]
+namespace signalRtest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder App)
+        {
+            App.MapSignalR();
+        }
+    }
+}
